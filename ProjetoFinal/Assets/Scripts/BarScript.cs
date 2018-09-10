0b0c67161ef34d47fe5eeb7c524a96ee18336160
LUCAS MATEUS DE OLIVEIRA <ALU201610484@uniritter.local>
2018-09-10 20:45:25 -0300

    HUD (botoes c animaçao e barra de vida funcional)

---
+++ ProjetoFinal/Assets/Scripts/BarScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour
+{
+
+    public Transform Bar;
+    public Slider barSlider;
+
+    public float currentResource;
+
+    public float maxResource;
+
+    public float BarYOffset = 2;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            TestBar();
+        }
+    }
+
+    public void ChangeResource(int amount)
+    {
+        currentResource += amount;
+        currentResource = Mathf.Clamp(currentResource, 0, maxResource);
+        barSlider.value = (currentResource / maxResource)*100;
+        Debug.Log(barSlider.value);
+    }
+
+    //private void PositionBar()
+    //{
+    //    Vector3 currentPos = transform.position;
+
+    //    Bar.position = new Vector3(currentPos.x, currentPos.y + BarYOffset, currentPos.z);
+
+    //    Bar.LookAt(Camera.main.transform);
+    //}
+
+    private void TestBar()
+    {
+        Debug.Log("diminuindo vida");
+        ChangeResource(-5);
+    }
+}
---
+++ ProjetoFinal/Assets/Scripts/SceneMaganer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneMaganer : MonoBehaviour
+{
+    public Button buttonNewGame, buttonOptions, buttonExit, buttonVoltaMenu;
+
+    void Start()
+    {
+        buttonNewGame.gameObject.SetActive(true);
+        buttonOptions.gameObject.SetActive(true);
+        buttonExit.gameObject.SetActive(true);
+        buttonVoltaMenu.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ButtonClick(string ButtonFunc)
+    {
+        if (ButtonFunc == "Start")
+        {
+            Debug.Log("start button");
+            //SceneManager.LoadScene(indice da cena)
+        }
+        else if (ButtonFunc == "Options")
+        {
+            Debug.Log("options button");
+            buttonNewGame.gameObject.SetActive(false);
+            buttonOptions.gameObject.SetActive(false);
+            buttonExit.gameObject.SetActive(false);
+            buttonVoltaMenu.gameObject.SetActive(true);
+        }
+        else if (ButtonFunc == "FazAlgo")
+        {
+            Debug.Log("faz algo button");
+            buttonNewGame.gameObject.SetActive(true);
+            buttonOptions.gameObject.SetActive(true);
+            buttonExit.gameObject.SetActive(true);
+            buttonVoltaMenu.gameObject.SetActive(false);
+        }
+        else if (ButtonFunc == "Exit")
+        {
+            Application.Quit();
+        }
+        else
+        {
+
+        }
+    }
+}
