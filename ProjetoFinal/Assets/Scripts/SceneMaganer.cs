ac50168f548d0a42978367f316ec8ba8b4d48a4d
Lucas <lucasmbsd@gmail.com>
2018-10-29 20:04:46 -0300

    Pause in game with HUD

--- ProjetoFinal/Assets/Scripts/SceneMaganer.cs
+++ ProjetoFinal/Assets/Scripts/SceneMaganer.cs
@@ -1,28 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneMaganer : MonoBehaviour
 {
-    public Button buttonNewGame, buttonOptions, buttonExit, buttonVoltaMenu;
+    public Button buttonNewGame, buttonOptions, buttonExit, buttonReturn, buttonResume, buttonMenu;
+    public Text pauseText;
     public Slider volumeSlider;
     public AudioSource audioData;
+    public bool gameIsPaused = false;
 
     void Start()
     {
-        buttonNewGame.gameObject.SetActive(true);
-        buttonOptions.gameObject.SetActive(true);
-        buttonExit.gameObject.SetActive(true);
-        buttonVoltaMenu.gameObject.SetActive(false);
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            buttonNewGame.gameObject.SetActive(true);
+            buttonOptions.gameObject.SetActive(true);
+            buttonExit.gameObject.SetActive(true);
+            buttonReturn.gameObject.SetActive(false);
+        }
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            pauseText.gameObject.SetActive(false);
+            buttonResume.gameObject.SetActive(false);
+            buttonOptions.gameObject.SetActive(false);
+            buttonMenu.gameObject.SetActive(false);
+            buttonReturn.gameObject.SetActive(false);
+        }
         audioData = GetComponent<AudioSource>();
         if (audioData != null) audioData.Play(0);
     }
 
     void Update()
     {
-       AudioListener.volume = volumeSlider.value;
+        if (volumeSlider != null)
+        {
+            AudioListener.volume = volumeSlider.value;
+        }
+       Pause();
     }
 
     public void ButtonClick(string ButtonFunc)
@@ -32,31 +47,94 @@ public class SceneMaganer : MonoBehaviour
             buttonNewGame.gameObject.SetActive(false);
             buttonOptions.gameObject.SetActive(false);
             buttonExit.gameObject.SetActive(false);
-            buttonVoltaMenu.gameObject.SetActive(false);
+            buttonReturn.gameObject.SetActive(false);
             SceneManager.LoadScene("Scene01", LoadSceneMode.Single);
             if (audioData != null) audioData.Stop();
         }
         else if (ButtonFunc == "Options")
         {
-            buttonNewGame.gameObject.SetActive(false);
-            buttonOptions.gameObject.SetActive(false);
-            buttonExit.gameObject.SetActive(false);
-            buttonVoltaMenu.gameObject.SetActive(true);
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                buttonNewGame.gameObject.SetActive(false);
+                buttonOptions.gameObject.SetActive(false);
+                buttonExit.gameObject.SetActive(false);
+                buttonReturn.gameObject.SetActive(true);
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                buttonResume.gameObject.SetActive(false);
+                buttonOptions.gameObject.SetActive(false);
+                buttonMenu.gameObject.SetActive(false);
+                buttonReturn.gameObject.SetActive(true);
+            }
         }
         else if (ButtonFunc == "Return")
         {
-            buttonNewGame.gameObject.SetActive(true);
-            buttonOptions.gameObject.SetActive(true);
-            buttonExit.gameObject.SetActive(true);
-            buttonVoltaMenu.gameObject.SetActive(false);
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                buttonNewGame.gameObject.SetActive(true);
+                buttonOptions.gameObject.SetActive(true);
+                buttonExit.gameObject.SetActive(true);
+                buttonReturn.gameObject.SetActive(false);
+            }
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                buttonResume.gameObject.SetActive(true);
+                buttonOptions.gameObject.SetActive(true);
+                buttonMenu.gameObject.SetActive(true);
+                buttonReturn.gameObject.SetActive(false);
+            }
         }
         else if (ButtonFunc == "Exit")
         {
             Application.Quit();
         }
+        else if (ButtonFunc == "Resume")
+        {
+            gameIsPaused = false;
+            Time.timeScale = 1.0f;
+            pauseText.gameObject.SetActive(false);
+            buttonResume.gameObject.SetActive(false);
+            buttonOptions.gameObject.SetActive(false);
+            buttonMenu.gameObject.SetActive(false);
+            buttonReturn.gameObject.SetActive(false);
+        }
+        else if (ButtonFunc == "Menu")
+        {
+            SceneManager.LoadScene("menu", LoadSceneMode.Single);
+        }
         else
         {
 
+        }
+    }
+    public void Pause()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (gameIsPaused)
+                {
+                    gameIsPaused = false;
+                    Time.timeScale = 1.0f;
+                    pauseText.gameObject.SetActive(false);
+                    buttonResume.gameObject.SetActive(false);
+                    buttonOptions.gameObject.SetActive(false);
+                    buttonMenu.gameObject.SetActive(false);
+                    buttonReturn.gameObject.SetActive(false);
+                }
+                else
+                {
+                    gameIsPaused = true;
+                    Time.timeScale = 0.0f;
+                    pauseText.gameObject.SetActive(true);
+                    buttonResume.gameObject.SetActive(true);
+                    buttonOptions.gameObject.SetActive(true);
+                    buttonMenu.gameObject.SetActive(true);
+                    buttonReturn.gameObject.SetActive(false);
+                }
+            }
         }
     }
 }
